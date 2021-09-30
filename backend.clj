7da2038bf5f4c5bff71ced9e5ebdd3b5a6fa107d
Rebecca Kelly <bk@ancilla.ca>
2021-09-30 18:05:55 -0400

    tidy up zeiat backend types

--- backend.clj
+++ backend.clj
@@ -5,24 +5,6 @@
     [schema.core :as s :refer [def defn defmethod defrecord defschema fn letfn]]
     ))
 
-(defschema BackendState s/Any)
-
-(defschema Chat s/Any)
-
-; LIST
-; - get: nothing
-; - need: irc name
-; - want: real name, topic, user count
-; NAMES
-; - get: irc channel name
-; - need: list of nicks on channel
-; WHO
-; - get: nothing
-; - need: nick, user, host, realname
-; JOIN
-; - get: irc channel name
-; - need: topic (can use real name instead), list of names
-
 (defschema ChannelName
   "An IRC-compatible channel name. Only #channels are supported."
   (s/constrained
@@ -76,22 +58,25 @@
 (defprotocol ZeiatBackend
   "A protocol that Zeiat uses to communicate with whatever backend you connect to it. Library users should supply something that implements this protocol to zeiat/run."
   (connect [this] ;- nil
+    ; TODO: this should take information about the registered user, so that the backend can
+    ; make decisions based on that, and should return human-readable information for the
+    ; frontend to display.
     "Connect to the backend. Called when a new user connects to Zeiat.")
   (disconnect [this] ;- nil
     "Disconnect from the backend. Called when a user quits or is otherwise disconnected.")
   (listChannels [this] ;- [Channel]
     "List all available channels. Called in response to a user's LIST command.")
-  (listUsers [this]
+  (listUsers [this] ;- [User]
     "List all available (DMable) users. Called in response to a user's WHO command.")
-  (listUnread [this]
+  (listUnread [this] ;- [Chat]
     "List all chats (users or channels) with unread messages. Called periodically to monitor for message traffic. Implementations can implement this however they want but note that just blindly returning all chats will result in a lot of unnecessary read-messages calls.")
-  (statChannel [this channel]
+  (statChannel [this channel] ;- Chat
     "Return information about a channel's users and read status.")
-  (listMembers [this channel]
+  (listMembers [this channel] ;- [User]
     "List all users in a given channel. See FIXME for the data shape. Called in response to NAMES or JOIN.")
-  (readMessages [this channel]
+  (readMessages [this channel] ;- [Message]
     "Return all messages from the given channel available in the backscroll. Implementors can limit this to only what's easily available if convenient (e.g. return only history that was autoloaded, not all history). Calling this should mark the chat as read.")
-  (readNewMessages [this channel]
+  (readNewMessages [this channel] ;- [Message]
     "As readMessages but should return only messages have not yet been read. Calling this should mark the chat as read.")
-  (writeMessage [this channel message]
-    "Send a message to the given channel or user. Calling this should mark the chat as read."))
+  (writeMessage [this channel message] ;- bool
+    "Send a message to the given channel or user. Calling this should mark the chat as read. Returns true if the message was successfully sent, false otherwise."))
