51a3c85611519f67c81a7cd7717104b0cfc59b6f
Rebecca Kelly <bk@ancilla.ca>
2022-10-15 18:57:57 -0400

    prettyprint translator.clj

--- src/zeiat/translator.clj
+++ src/zeiat/translator.clj
@@ -7,8 +7,7 @@
     [zeiat.types :refer [TranslatorState]]
     [zeiat.state :as statelib]
     [schema.core :as s :refer [def defn defmethod defrecord defschema fn letfn]]
-    [taoensso.timbre :as log]
-    ))
+    [taoensso.timbre :as log]))
 
 (defn startup! :- TranslatorState
   "Called when the translator is created. Currently a no-op."
@@ -25,32 +24,32 @@
   (reduce
     (fn [state info]
       (let [chat (:name info)]
-      (log/debug "Fetching new messages for" chat
-                 "with cached timestamp" (statelib/read-cache state chat)
-                 "and info from backend" info)
-      (log/trace "fetch-new-messages" chat (:last-seen state))
-      (let [{:keys [last-seen outgoing]} (statelib/read-cache state chat)
-            messages (backend/read-messages-since (:backend state) chat last-seen)
-            [n-removed displayable-messages] (filter-n-self-messages outgoing messages)]
-        (run! privmsg displayable-messages)
-        (log/debug "Updating last-seen value for", chat, "to", (:timestamp (last messages) last-seen), "from", last-seen)
-        (log/debug "Reducing outgoing counter for" chat "by" n-removed "currently" outgoing)
-        (when (> n-removed outgoing)
-          (log/warn chat "n-removed value greater than outgoing:" n-removed outgoing))
-        ; If messages is empty, (:timestamp (last messages)) is nil, so in that case
-        ; we default to the value we have recorded already -- otherwise we would end up
-        ; erasing it and fetching the entire history next time.
-        (statelib/write-cache
-          state chat
-          ; TODO: (last messages) doesn't always work here because some backends (e.g. discord) send messages out-of-order
-          ; in ways that still read reasonably but mean that the last message isn't always the one with the highest TS
-          ; we should instead select the max timestamp from the messages we fetched
-          :last-seen (:timestamp (last messages) last-seen)
-          :outgoing (max 0 (- outgoing n-removed)))
-        ))
-      )
-        ; TODO anything that results in a channel being re-statted should also result
-        ; in a new NAMES line being sent to the client
+       (log/debug "Fetching new messages for" chat
+                  "with cached timestamp" (statelib/read-cache state chat)
+                  "and info from backend" info)
+       (log/trace "fetch-new-messages" chat (:last-seen state))
+       (let [{:keys [last-seen outgoing]} (statelib/read-cache state chat)
+             messages (backend/read-messages-since (:backend state) chat last-seen)
+             [n-removed displayable-messages] (filter-n-self-messages outgoing messages)]
+         (run! privmsg displayable-messages)
+         (log/debug "Updating last-seen value for", chat, "to", (:timestamp (last messages) last-seen), "from", last-seen)
+         (log/debug "Reducing outgoing counter for" chat "by" n-removed "currently" outgoing)
+         (when (> n-removed outgoing)
+           (log/warn chat "n-removed value greater than outgoing:" n-removed outgoing))
+         ; If messages is empty, (:timestamp (last messages)) is nil, so in that case
+         ; we default to the value we have recorded already -- otherwise we would end up
+         ; erasing it and fetching the entire history next time.
+         (statelib/write-cache
+           state chat
+           ; TODO: (last messages) doesn't always work here because some backends (e.g. discord) send messages out-of-order
+           ; in ways that still read reasonably but mean that the last message isn't always the one with the highest TS
+           ; we should instead select the max timestamp from the messages we fetched
+           :last-seen (:timestamp (last messages) last-seen)
+           :outgoing (max 0 (- outgoing n-removed))))))
+
+
+         ; TODO anything that results in a channel being re-statted should also result
+         ; in a new NAMES line being sent to the client
     state chats))
 
 (defn- interesting?
@@ -72,9 +71,14 @@
       ; treat it as if we had no cache entry.
       (nil? (:last-seen chat)) (= :unread status)
       ; Finally, if we do have a cache entry *and* the backend returned something we can compare it to, do so.
-      :else (not= last-seen (:last-seen chat))
-      )))
+      :else (not= last-seen (:last-seen chat)))))
 
+; TODO: we probably want two agents:
+; - a frontend agent that sends and recieves IRC messages and manages the sendq. Messages that can be replied to without
+;   involving the backend will be. Messages that need to go to the backend get sent off with agent messages.
+; - a backend agent that does polling of the backend and handles communication with it
+; this needs (and doesn't have yet) a proper design to handle the fact that the state will need to be split across these
+; agents and stuff...it's going to be tricky.
 (defn- poll
   [{:keys [backend socket] :as state}]
   (if (.isClosed socket)
@@ -100,8 +104,8 @@
           (fn [state [name last-seen]]
             (log/debug "Recording initial last-seen value of" last-seen "for" name)
             (statelib/write-cache state name :last-seen last-seen))
-          state updates)
-        ))))
+          state updates)))))
+
 
 (defn connect! :- s/Str
   "Called when user registration completes successfully. Should connect to the backend. Returns the info string provided by the backend."
@@ -123,4 +127,4 @@
      (.println writer (str ":Zeiat ERROR :" reason))
      (.close socket))
    (backend/disconnect backend)
-  state))
+   state))
